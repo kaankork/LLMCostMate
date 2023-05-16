dc45ad5165ff65c43ca8de9284f99f3938b7b588
kaankork <kaankorkmaz93@gmail.com>
2023-05-17 02:52:03 +0300

    added token calculator for aleph

--- util.py
+++ util.py
@@ -1,7 +1,25 @@
 import tiktoken
+import os
+from aleph_alpha_client import Client, TokenizationRequest, DetokenizationRequest
+from dotenv import load_dotenv
+
+load_dotenv()
 
 def num_tokens_from_string(string: str, model_name: str) -> int:
     """Returns the number of tokens in a text string."""
     encoding = tiktoken.encoding_for_model(model_name)
     num_tokens = len(encoding.encode(string))
     return num_tokens
+
+def num_tokens_from_string_aleph_alpha(string: str, model_name: str) -> int:
+    """Returns the number of tokens in a text string."""
+    client = Client(token=os.getenv("AA_TOKEN"))
+    params = {
+        "prompt": string,
+        "tokens": True,
+        "token_ids": False
+    }
+    tokenization_request = TokenizationRequest(**params)
+    tokenization_response = client.tokenize(request=tokenization_request, model=f"luminous-{model_name}")
+    tokens = tokenization_response.tokens
+    return len(tokens)
